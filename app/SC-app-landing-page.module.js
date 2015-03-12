7c519e17f43d72f4b2e4f9e6145eff1fd60975fe
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:16:58 +0000

    initial commit

---
+++ app/SC-app-landing-page.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-landing-page
+ * @description
+ *
+ * Provides the app with the ability to display landing page content and features
+ */
+angular
+  .module('SC-app-landing-page');
